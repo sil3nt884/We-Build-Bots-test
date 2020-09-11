4c1ea669c1d3feb88dde4dcc15606f68fa0b6b9a
ricky.luu <sil3nt884@hotmail.co.uk>
2020-09-11 12:03:29 +0100

    Added app.js for express setup

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
-const http = require('http')
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const indexRouter = require('./routers/index')
-const path = require('path')
+const main = async  () => {
+	const http = require('http')
+	const express = require('express')
+	const bodyParser = require('body-parser')
+	const app = express()
+	const indexRouter = require('./routers/index')
+	const path = require('path')
+	const textParser = require('./textParser')
+	await textParser.dataReady()
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(bodyParser.json())
+	app.use(express.urlencoded({extended: true}))
+	app.use(express.json())
+	app.use(bodyParser.json())
+	app.set('textPaser', textParser)
+	app.use('/', indexRouter)
 
-app.use('/', indexRouter)
+	const port = parseInt(process.env.httpPort) || 3000
+	http.createServer(app).listen(port)
+	console.log('server listening on', port)
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500)
+	})
 
-const port = parseInt(process.env.httpPort) || 3000
-http.createServer(app).listen(port)
-
-app.use(function(err, req, res, next) {
-	res.status(err.status || 500)
-})
+}
+main()
