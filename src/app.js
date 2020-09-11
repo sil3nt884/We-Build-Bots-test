84f1538e40e9dc9de8609bfc8e77e866c8fd5786
ricky.luu <sil3nt884@hotmail.co.uk>
2020-09-11 12:12:29 +0100

    Fix linting

--- src/app.js
+++ src/app.js
@@ -1,24 +1,23 @@
-const main = async  () => {
-	const http = require('http')
-	const express = require('express')
-	const bodyParser = require('body-parser')
-	const app = express()
-	const indexRouter = require('./routers/index')
-	const textParser = require('./textParser')
-	await textParser.dataReady()
+const main = async () => {
+  const http = require('http')
+  const express = require('express')
+  const bodyParser = require('body-parser')
+  const app = express()
+  const indexRouter = require('./routers/index')
+  const textParser = require('./textParser')
+  await textParser.dataReady()
 
-	app.use(express.urlencoded({extended: true}))
-	app.use(express.json())
-	app.use(bodyParser.json())
-	app.set('textPaser', textParser)
-	app.use('/', indexRouter)
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(bodyParser.json())
+  app.set('textPaser', textParser)
+  app.use('/', indexRouter)
 
-	const port = parseInt(process.env.httpPort) || 3000
-	http.createServer(app).listen(port)
-	console.log('server listening on', port)
-	app.use(function(err, req, res, next) {
-		res.status(err.status || 500)
-	})
-
+  const port = parseInt(process.env.httpPort) || 3000
+  http.createServer(app).listen(port)
+  console.log('server listening on', port)
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+  })
 }
 main()
--- src/textParser.js
+++ src/textParser.js
@@ -1,24 +1,23 @@
-const fsPromise = require('fs').promises;
-const Event = require('events');
+const fsPromise = require('fs').promises
+const Event = require('events')
 const eventEmitter = new Event()
 
-
 const getFilesNames = async (resourcePath) => {
-	return await fsPromise.readdir(resourcePath);
-};
+  return await fsPromise.readdir(resourcePath)
+}
 
 const loadResources = async (files, resourcePath) => {
-	return files.filter(fName => fName.endsWith('.txt')).
-			map(fileName => ({
-				[fileName]: fsPromise.readFile(`${resourcePath}/${fileName}`, 'utf8'),
-			})).
-			reduce((acc, curr) => Object.assign(acc, curr));
-};
+  return files.filter(fName => fName.endsWith('.txt'))
+    .map(fileName => ({
+      [fileName]: fsPromise.readFile(`${resourcePath}/${fileName}`, 'utf8')
+    }))
+    .reduce((acc, curr) => Object.assign(acc, curr))
+}
 
 const createNamesArray = async (firstNamesFile) => {
-	const names = await firstNamesFile;
-	return names.split(/\n/);
-};
+  const names = await firstNamesFile
+  return names.split(/\n/)
+}
 
 /*
   splitting on double lines here also
@@ -27,27 +26,26 @@ const createNamesArray = async (firstNamesFile) => {
  */
 
 const splitTextIntoArray = (text) => {
-	return text.split(/\n/);
-};
+  return text.split(/\n/)
+}
 
 const isName = (word, name) => {
-	return word.indexOf(name)
-};
+  return word.indexOf(name)
+}
 
 const createHash = (names) => {
-	return names.filter(e => e.trim()).
-			map(name => ({[name]: name})).
-			reduce((arr, current) => Object.assign(arr, current));
+  return names.filter(e => e.trim())
+    .map(name => ({ [name]: name }))
+    .reduce((arr, current) => Object.assign(arr, current))
 }
 
 const secondCheckName = (word, hash, name) => {
-	const isInHash = word in hash
-	if(isInHash) {
-		return ''
-	}
-	else {
-		return name
-	}
+  const isInHash = word in hash
+  if (isInHash) {
+    return ''
+  } else {
+    return name
+  }
 }
 
 /**
@@ -62,75 +60,73 @@ const secondCheckName = (word, hash, name) => {
  */
 
 const checkForNames = (wordArray, names, namesHash) => {
-	let namesArray = []
-	wordArray.forEach((word) => {
-		namesArray.push(names.filter(name => {
-			const isNameBool = isName(word, name)
-			if (isNameBool) {
-				const nameLength = name.length
-				const nextWordIndex = word.indexOf(name) + 1
-				if (nextWordIndex > 1) {
-					const nextWord = word.substring(nextWordIndex + nameLength).
-							split(/\s+/).
-							filter(w => w !== '')[0]
-					return secondCheckName(nextWord, namesHash, name)
-				}
-			}
-		}))
-	})
-	return namesArray
-	.reduce((acc, current) => acc.concat(current))
+  const namesArray = []
+  wordArray.forEach((word) => {
+    namesArray.push(names.filter(name => {
+      const isNameBool = isName(word, name)
+      if (isNameBool) {
+        const nameLength = name.length
+        const nextWordIndex = word.indexOf(name) + 1
+        if (nextWordIndex > 1) {
+          const nextWord = word.substring(nextWordIndex + nameLength)
+            .split(/\s+/)
+            .filter(w => w !== '')[0]
+          return secondCheckName(nextWord, namesHash, name)
+        }
+      }
+    }))
+  })
+  return namesArray
+    .reduce((acc, current) => acc.concat(current))
 }
 
-
 const prepareCheckNames = async (namesArray, text) => {
-	const names = await namesArray;
-	const textContent = await text;
-	const wordArray = splitTextIntoArray(textContent).filter(e => e);
-	const namesHash = createHash(names)
-	return checkForNames(wordArray, names, namesHash);
-};
+  const names = await namesArray
+  const textContent = await text
+  const wordArray = splitTextIntoArray(textContent).filter(e => e)
+  const namesHash = createHash(names)
+  return checkForNames(wordArray, names, namesHash)
+}
 const createCollectionOfNames = (files) => {
-	const knownFiles = ['first-names.txt', 'oliver-twist.txt'];
-	const namesArray = createNamesArray(files[knownFiles[0]]);
-	return prepareCheckNames(namesArray, files[knownFiles[1]]);
-};
+  const knownFiles = ['first-names.txt', 'oliver-twist.txt']
+  const namesArray = createNamesArray(files[knownFiles[0]])
+  return prepareCheckNames(namesArray, files[knownFiles[1]])
+}
 
 const createNameCountJSON = (names) => {
-	let namesCount = {}
-	names.forEach(name => {
-		if(!namesCount[name]) {
-			namesCount[name] = 1
-		}
-		else {
-			namesCount[name] = namesCount[name]  +1
-		}
-	})
-	return namesCount
+  const namesCount = {}
+  names.forEach(name => {
+    if (!namesCount[name]) {
+      namesCount[name] = 1
+    } else {
+      namesCount[name] = namesCount[name] + 1
+    }
+  })
+  return namesCount
 }
 
 let namesCountObject
 const main = async () => {
-	const startTime = Date.now()
-	console.log('started')
-	const resourcePath = 'res';
-	const filesNames = await getFilesNames(resourcePath);
-	const files = await loadResources(filesNames, resourcePath);
-	const names = await createCollectionOfNames(files);
-	namesCountObject = createNameCountJSON(names)
-	eventEmitter.emit('name data ready')
-	console.log('data ready time', (Date.now() - startTime) / 1000.0)
-};
-main();
+  const startTime = Date.now()
+  console.log('started')
+  const resourcePath = 'res'
+  const filesNames = await getFilesNames(resourcePath)
+  const files = await loadResources(filesNames, resourcePath)
+  const names = await createCollectionOfNames(files)
+  namesCountObject = createNameCountJSON(names)
+  eventEmitter.emit('name data ready')
+  console.log('data ready time', (Date.now() - startTime) / 1000.0)
+}
+main()
 
 const dataReady = () => {
-	return new Promise(resolve => {
-		eventEmitter.on('name data ready', () => {
-			resolve()
-		})
-	})
+  return new Promise(resolve => {
+    eventEmitter.on('name data ready', () => {
+      resolve()
+    })
+  })
 }
 
 const getNameCount = () => namesCountObject
 
-module.exports = {dataReady, getNameCount }
+module.exports = { dataReady, getNameCount }
